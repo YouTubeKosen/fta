0774e7752d423999a1dbc378f226277ef5066ff0
YouTubeKosen <private.kosen@gmail.com>
2023-04-04 20:11:36 +0900

    add space

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
+
 #define ADD(a,b) a+b
 
 int main(void) {
     uint32_t a = 1, b = 2, c =3;
-    printf("%d+%d=%d\n", a, b, ADD(a, b));
-    printf("(%d+%d)*%d=%d\n", a, b, c, ADD(1,2) * 3);
+
+    printf("%d + %d = %d\n", a, b, ADD(a, b));
+    printf("(%d + %d)*%d = %d\n", a, b, c, ADD(1,2) * 3);
     return 0;
 }
